1c7e07bf188664e4b7738e03486d2deb5807e643
Bert Witzel <bwitzel82@gmail.com>
2021-06-05 10:35:04 -0400

    finishing touches

--- js/app.js
+++ js/app.js
@@ -4,89 +4,144 @@ let rank = '';
 
 const main = document.querySelector('main');
 const quiz = [
-    'Which character says "D\'oh"?',
-    'Which character rides a skateboard?',
-    'Fill in this sentence: "Mr.______owns the Nuclear Power Plant.',
-    'Which character plays the saxophone?',
-    'Which character is Principal of the elementary school?'
+    { 
+        question: 'Which character says "D\'oh"?',
+        choices: {
+            a: 'Marge',
+            b: 'Homer',
+            c: 'Krusty the Klown'
+        },
+        answer: 'b'
+    },
+    {
+        question: 'Which character rides a skateboard?',
+        choices: {
+            a: 'Milhouse',
+            b: 'Chief Wiggum',
+            c: 'Bart'
+        },
+        answer: 'c'
+    },
+    {
+        question: 'Which character owns the Nuclear Power Plant?',
+        choices: {
+            a: 'Mr. Burns',
+            b: 'Kent Brockman',
+            c: 'Mayor Quimby'
+        },
+        answer: 'a'
+    },
+    {
+        question: 'Which character plays the saxophone?',
+        choices: {
+            a: 'Uter',
+            b: 'Ralph Wiggum',
+            c: 'Lisa'
+        },
+        answer: 'c'
+    },
+    {
+        question: 'Which character is Principal of the elementary school?',
+        choices: {
+            a: 'Ned Flanders',
+            b: 'Seymour Skinner',
+            c: 'Mrs. Krabapple'
+        },
+        answer: 'b'
+    }
 ];
 
-// generate questions
-function generateQuestion(question, index) {
-    const num = parseInt(index);
+// generate question function
+function generateQuestion(array, index) {
+    const num = parseInt(index) + 1;
     const quizQuestion = `
         <div>
-            <label>${num + 1}. ${question}</label>
-            <input text="text" name="question-input" id="question-${index}">
+            <h2>${num}. ${array.question}</h2>
+            <div class="question">
+                <input type="radio" name="question-${num}" class="question-${index}" value="a">
+                <label>${array.choices.a}<label>
+            </div>
+            <div class="question">
+                <input type="radio" name="question-${num}" class="question-${index}" value="b">
+                <label>${array.choices.b}</label>
+            </div>
+            <div class="question">
+                <input type="radio" name="question-${num}" class="question-${index}" value="c">
+                <label>${array.choices.c}</label>
+            </div> 
         </div>
     `;
     main.insertAdjacentHTML('beforeend', quizQuestion);
 }
 
+// loop to generate questions html
 for (let i = 0; i < quiz.length; i++){
-    console.log('whats up');
     generateQuestion(quiz[i], [i]);
 }
 
+// insert submit button
+const submitBtn = `
+<div class="button-div">
+    <button id="submitBtn">Submit</button>
+</div>`;
+main.insertAdjacentHTML('afterend', submitBtn);
+
 // check answers
-
-
-// main.innerHTML = `
-
-// `;
-
-// const questionOne = prompt();
-// if ( questionOne.toUpperCase() === 'HOMER' ) {
-//        score += 1; 
-// }
-// const questionTwo = prompt();
-// if ( questionTwo.toUpperCase() === 'BART' ) {
-//        score += 1;
-// }   
-// const questionThree = prompt();
-// if ( questionThree.toUpperCase() === 'BURNS' ) {
-//        score += 1;
-// }   
-// const questionFour = prompt();
-// if ( questionFour.toUpperCase() === 'LISA' ) {
-//        score += 1;
-// }   
-// const questionFive = prompt();
-// if ( questionFive.toUpperCase() === 'PRINCIPAL SKINNER' || questionFive.toUpperCase() === 'SKINNER' || questionFive.toUpperCase() === 'SEYMOUR SKINNER' ) {
-//        score += 1;
-// }                        
-     
-
-/*
-  5. Rank player based on number of correct answers
-   - 5 correct = Gold
-   - 3-4 correct = Silver
-   - 1-2 correct = Bronze
-   - 0 correct = No crown
-*/
+function checkHelper(question, index) {
+    for (let i = 0; i <question.length; i++) {
+        if (question[i].checked) {
+            const answer = question[i].value;
+            if (answer === quiz[index].answer){
+                score += 1;
+            }
+        }
+    }
+}
                            
-// if ( score === 5 ) {
-//    rank = 'Gold';  
-//    main.innerHTML = `<h2>You got ${score} answers correct</h2>
-//                   <p>You have earned: ${rank}</p>
-//                   <img src="https://vignette.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png/revision/latest?cb=20140126234206" 
-//                   alt="Homer Simpson">` ;
-// } else if ( score >= 3 ) {
-//   rank = 'Silver'; 
-//   main.innerHTML = `<h2>You got ${score} answers correct</h2>
-//                   <p>You have earned: ${rank}</p>
-//                   <img src="https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20190409004756" 
-//                   alt="Bart Simpson">`;
-// } else if ( score >= 1 ) {
-//    rank = 'Bronze'; 
-//    main.innerHTML = `<h2>You got ${score} answers correct</h2>
-//                   <p>You have earned: ${rank}</p>
-//                   <img src="https://vignette.wikia.nocookie.net/simpsons/images/d/d5/Lisa_Simpson_official.png/revision/latest?cb=20190409004811"
-//                   alt="Lisa Simpson">`; 
-// } else {
-//   rank = 'No Crown';    
-//   main.innerHTML = `<h2>You got ${score} answers correct</h2>
-//                   <p>You have earned: ${rank}</p>
-//                   <img src="https://vignette.wikia.nocookie.net/simpsons/images/a/a7/Montgomery_Burns.png/revision/latest?cb=20100602062705"
-//                   alt="Mr. Burns">`;
-// }
+function checkAnswers() {
+    const question0 = document.querySelectorAll('.question-0');
+    const question1 = document.querySelectorAll('.question-1');
+    const question2 = document.querySelectorAll('.question-2');
+    const question3 = document.querySelectorAll('.question-3');
+    const question4 = document.querySelectorAll('.question-4');
+    const btnDiv = document.querySelector('.button-div');
+    checkHelper(question0, 0);
+    checkHelper(question1, 1);
+    checkHelper(question2, 2);
+    checkHelper(question3, 3);
+    checkHelper(question4, 4);
+    if ( score === 5 ) {
+        rank = 'excellent';  
+        btnDiv.innerHTML = `<div class="results">
+                            <p>You got ${score} answers correct</p>
+                            <p>You have ${rank} knowledge of the Simpsons</p>
+                            </div>
+                            `;
+     } else if ( score >= 3 ) {
+       rank = 'good'; 
+       btnDiv.innerHTML = `<div class="results">
+       <p>You got ${score} answers correct</p>
+       <p>You have ${rank} knowledge of the Simpsons</p>
+       </div>
+       `;
+     } else if ( score >= 1 ) {
+        rank = 'average'; 
+        btnDiv.innerHTML = `<div class="results">
+        <p>You got ${score} answers correct</p>
+        <p>You have ${rank} knowledge of the Simpsons</p>
+        </div>
+        `;
+     } else {
+       rank = 'poor';    
+       btnDiv.innerHTML = `<div class="results">
+       <p>You got ${score} answers correct</p>
+       <p>You have ${rank} knowledge of the Simpsons. Watch more episodes!</p>
+       </div>
+       `;
+     }
+}
+
+document.querySelector('#submitBtn').addEventListener('click', () => {
+    checkAnswers();   
+});
+
